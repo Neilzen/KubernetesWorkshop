b25e56ab39bd2c12b1d730aa4f05565c1989ee3b
Neilzen <detorres_neilzen@yahoo.com>
2020-12-03 01:43:40 +0000

    Add Monitor

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -3,9 +3,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import ch.qos.logback.classic.Logger;
+
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/pages")
 public class PageController {
+
+    Logger logger =(Logger)LoggerFactory.getLogger(this.getClass());
+
     private IPageRepository pageRepository;
     public PageController(IPageRepository pageRepository)
     {
@@ -16,14 +23,7 @@ public class PageController {
         Page newPage= pageRepository.create(page);
         return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
     }
-    @GetMapping("{id}")
-    public ResponseEntity<Page> read(@PathVariable long id) {
-        Page page = pageRepository.read(id);
-        if(page!=null)
-            return new ResponseEntity<Page>(page, HttpStatus.OK);
-        else
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-    }
+
     @GetMapping
     public ResponseEntity<List<Page>> list() {
         List<Page> pages= pageRepository.list();
@@ -41,5 +41,20 @@ public class PageController {
     public ResponseEntity delete(@PathVariable long id) {
         pageRepository.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Page> read(@PathVariable long id) {
+
+        logger.info("READ-INFO:Fetching page with id = " + id);
+        logger.debug("READ-DEBUG:Fetching page with id = " + id);
+
+        Page page = pageRepository.read(id);
+        if(page!=null)
+            return new ResponseEntity<Page>(page, HttpStatus.OK);
+        else {
+            logger.error("READ-ERROR:Could not find page with id = " + id);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
